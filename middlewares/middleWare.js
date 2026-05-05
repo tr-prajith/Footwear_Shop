88b3318de4f2a68c3c3384a580e2e6763c7d969a
prajith <prajithappu1357@gmail.com>
2026-05-05 11:38:51 +0530

    new commit added

--- middlewares/middleWare.js
+++ middlewares/middleWare.js
@@ -1,2 +1,20 @@
 const jwt = require('jsonwebtoken')
-const authMiddleware = require('../middlewares/middleWare')
+const authMiddleware = async (req, res, next) => {
+    const header = req.header('Authorization')
+    console.log(header);
+    if (!header) {
+        return res.status(401).json({ msg: "No Token, Authorization Denied" })
+    }
+    try {
+        const token = header.split(" ")[1]
+        const decodedToken = jwt.verify(token, process.env.SECRET_KEY)
+        req.user = decodedToken
+        next()
+    } catch (error) {
+        return res.status(401).json({ msg: "Invalid Token" })
+    }
+
+}
+
+module.exports = authMiddleware
+
